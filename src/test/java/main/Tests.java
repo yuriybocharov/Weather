f6237c683dcfbb5d7e3c357bfae04fb2b94518e8
yuriybocharov <bocharov0682@gmail.com>
2019-10-31 11:24:18 +0200

    added test description

--- src/test/java/main/Tests.java
+++ src/test/java/main/Tests.java
@@ -23,13 +23,13 @@ public class Tests extends BaseWebDriver {
     }
 
 
-    @Test(priority = 1)
+    @Test(priority = 1, description = "goes to the mail")
     public void login() throws InterruptedException {
         LoginPage loginPage = new LoginPage (driver, wait);
         loginPage.loginToGmail (Variables.LOGIN, Variables.PASSWORD);
     }
 
-    @Test(priority = 2, dependsOnMethods = "login")
+    @Test(priority = 2, dependsOnMethods = "login", description = "sends a letter describing the weather")
     public void sendLetter() throws Exception {
         try {
             text = sendMassege ();
@@ -43,7 +43,8 @@ public class Tests extends BaseWebDriver {
 
     }
 
-    @Test(priority = 3, dependsOnMethods = "sendLetter")
+    @Test(priority = 3, dependsOnMethods = "sendLetter", description = "in the mail we go to the Inbox folder we " +
+            "find the letter by the header and check the text of the letter so that it was sent")
     public void check() throws Exception {
         CheckLetter checkLetter = new CheckLetter (driver);
         checkLetter.letterSearch ();
